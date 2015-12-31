22c0eeed16afcde08b6cb1a03a3d534d1f883fef
Brian Bader <brianbader@mail.com>
2015-12-31 21:49:33 +0000

    version 0.1.2

--- R/gpd.R
+++ R/gpd.R
@@ -1,7 +1,9 @@
 #' The Generalized Pareto Distribution (GPD)
 #'
-#' Density, distribution function, quantile function and random number generation for the Generalized Pareto
+#' Density, distribution function, quantile function and random number generation for the generalized pareto
 #' distribution with location, scale, and shape parameters.
+#' @name gpd
+#' @rdname gpd
 #' @param x Vector of observations.
 #' @param q Vector of quantiles.
 #' @param p Vector of probabilities.
@@ -12,59 +14,26 @@
 #' @param log.d Logical; if TRUE, the log density is returned.
 #' @param lower.tail Logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
 #' @param log.p Logical; if TRUE, probabilities p are given as log(p).
-#' @examples dgpd(2:4, 1, 0.5, 0.8)
-#' @examples pgpd(2:4, 1, 0.5, 0.8)
-#' @examples qgpd(seq(0.9, 0.6, -0.1), 2, 0.5, 0.8)
-#' @examples rgpd(6, 1, 0.5, 0.8)
-#' @examples p <- (1:9)/10
-#' @examples pgpd(qgpd(p, 1, 2, 0.8), 1, 2, 0.8)
-#' @examples ## [1] 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+#' @examples
+#' dgpd(2:4, 1, 0.5, 0.8)
+#' pgpd(2:4, 1, 0.5, 0.8)
+#' qgpd(seq(0.9, 0.6, -0.1), 2, 0.5, 0.8)
+#' rgpd(6, 1, 0.5, 0.8)
+#' p <- (1:9)/10
+#' pgpd(qgpd(p, 1, 2, 0.8), 1, 2, 0.8)
+#' ## [1] 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
 #'
-#' @details The Genralized Pareto distribution function is given (Pickands, 1975)
+#' @details The generalized pareto distribution function is given (Pickands, 1975)
 #' by \deqn{H(y) = 1 - \Big[1 + \frac{\xi (y - \mu)}{\sigma}\Big]^{-1/\xi}} defined
 #' on \eqn{\{y : y > 0, (1 + \xi (y - \mu) / \sigma) > 0 \}}, with location \eqn{\mu},
-#' scale \eqn{\sigma}, and shape parameter \eqn{\xi}.
+#' scale \eqn{\sigma > 0}, and shape parameter \eqn{\xi}.
 #'
 #' @references Pickands III, J. (1975). Statistical inference using extreme order statistics. Annals of Statistics, 119-131.
-#'
+NULL
+
+
 #' @rdname gpd
 #' @export
-rgpd <- function(n, loc = 0, scale = 1, shape = 0) {
-  if(min(scale) <= 0)
-    stop("invalid scale")
-  if((length(loc) != 1 & length(scale) != 1) | (length(loc) != 1 & length(shape) != 1) | (length(scale) != 1 & length(shape) != 1))
-    stop("only one parameter argument can be a vector")
-  if(n > 1 & (length(loc) != 1 | length(scale) != 1 | length(shape) != 1))
-    stop("cannot have a vector of parameters AND observations")
-  qgpd(runif(n), loc, scale, shape)
-}
-
-
-#'@rdname gpd
-#'@export
-pgpd <- function(q, loc = 0, scale = 1, shape = 0, lower.tail = TRUE, log.p = FALSE) {
-  if(min(scale) <= 0)
-    stop("invalid scale")
-  if((length(loc) != 1 & length(scale) != 1) | (length(loc) != 1 & length(shape) != 1) | (length(scale) != 1 & length(shape) != 1))
-    stop("only one parameter argument can be a vector")
-  if(length(q) > 1 & (length(loc) != 1 | length(scale) != 1 | length(shape) != 1))
-    stop("cannot have vectorized parameters and quantiles")
-  if(length(shape) == 1)
-    shape <- rep(shape, max(length(q), length(loc), length(scale)))
-  q <- pmax(q, loc)
-  q <- ifelse(shape >= 0, q, pmin(q, (loc - scale/shape)))
-  w <- (q - loc) / scale
-  p <- ifelse(shape == 0, 1 - exp(-w), 1 - exp((-1/shape)*log1p(w*shape)))
-  if (!lower.tail)
-    p <- 1 - p
-  if (log.p)
-    p <- log(p)
-  p
-}
-
-
-#'@rdname gpd
-#'@export
 dgpd <- function(x, loc = 0, scale = 1, shape = 0, log.d = FALSE) {
   if(min(scale) <= 0)
     stop("invalid scale")
@@ -86,8 +55,21 @@ dgpd <- function(x, loc = 0, scale = 1, shape = 0, log.d = FALSE) {
 }
 
 
-#'@rdname gpd
-#'@export
+#' @rdname gpd
+#' @export
+rgpd <- function(n, loc = 0, scale = 1, shape = 0) {
+  if(min(scale) <= 0)
+    stop("invalid scale")
+  if((length(loc) != 1 & length(scale) != 1) | (length(loc) != 1 & length(shape) != 1) | (length(scale) != 1 & length(shape) != 1))
+    stop("only one parameter argument can be a vector")
+  if(n > 1 & (length(loc) != 1 | length(scale) != 1 | length(shape) != 1))
+    stop("cannot have a vector of parameters AND observations")
+  qgpd(runif(n), loc, scale, shape)
+}
+
+
+#' @rdname gpd
+#' @export
 qgpd <- function(p, loc = 0, scale = 1, shape = 0, lower.tail = TRUE, log.p = FALSE) {
   if (log.p)
     p <- exp(p)
@@ -105,3 +87,27 @@ qgpd <- function(p, loc = 0, scale = 1, shape = 0, lower.tail = TRUE, log.p = FA
     shape <- rep(shape, max(length(p), length(loc), length(scale)))
   ifelse(shape == 0, loc - scale * log(p), loc + scale * expm1(-shape * log(p)) / shape)
 }
+
+
+#' @rdname gpd
+#' @export
+pgpd <- function(q, loc = 0, scale = 1, shape = 0, lower.tail = TRUE, log.p = FALSE) {
+  if(min(scale) <= 0)
+    stop("invalid scale")
+  if((length(loc) != 1 & length(scale) != 1) | (length(loc) != 1 & length(shape) != 1) | (length(scale) != 1 & length(shape) != 1))
+    stop("only one parameter argument can be a vector")
+  if(length(q) > 1 & (length(loc) != 1 | length(scale) != 1 | length(shape) != 1))
+    stop("cannot have vectorized parameters and quantiles")
+  if(length(shape) == 1)
+    shape <- rep(shape, max(length(q), length(loc), length(scale)))
+  q <- pmax(q, loc)
+  q <- ifelse(shape >= 0, q, pmin(q, (loc - scale/shape)))
+  w <- (q - loc) / scale
+  p <- ifelse(shape == 0, 1 - exp(-w), 1 - exp((-1/shape)*log1p(w*shape)))
+  if (!lower.tail)
+    p <- 1 - p
+  if (log.p)
+    p <- log(p)
+  p
+}
+
